04896f6dd5e312bd02a1a638090a7569aae9b267
pehcy <cheeyung.peh@gmail.com>
2022-08-01 00:08:31 +0800

    added while loop to keep alive

---
+++ README.md
@@ -0,0 +1,12 @@
+## What is AIMD?
+
+In TCP protocol, additive increase multiplicative decreasing is a 
+bottleneck control algorithm.
+
+## How to run client server?
+once you started the client server, you can use `telnet` to connect 
+to the client server.
+```bash
+telnet 127.0.0.1 8080
+# sample input 
+```
--- client.c
+++ client.c
@@ -17,6 +17,7 @@ int main(int argc, char * argv[])
     printf("Creating server socket");
     
     int sockfd, new_sockfd, connfd, len;
+    int sin_size;
     struct sockaddr_in servaddr, client_addr;
     struct hostent *hp;
     int recv_length=1, yes=1;
@@ -31,10 +32,6 @@ int main(int argc, char * argv[])
 
     bzero((char *)&servaddr, sizeof(servaddr));
 
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(__PORT_ADDRESS);
-
     // Forcefully attaching socket to the port 8080
     if (setsockopt(sockfd, SOL_SOCKET, 
             SO_REUSEADDR | SO_REUSEPORT, &yes, sizeof(int)) == -1) 
@@ -43,11 +40,42 @@ int main(int argc, char * argv[])
         exit(EXIT_FAILURE);
     }
 
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(__PORT_ADDRESS);
+    memset(&(servaddr.sin_zero), '\0', 8);
+
+    if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(struct sockaddr)) == -1)
+    {
+        perror("binding to socket");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(sockfd, 5) == -1)
+    {
+        printf("still listening on socket: 127.0.0.1:%d", __PORT_ADDRESS);
+    }
+    
+    printf("\n success");
+
     while(1)
     {
-        int sin_size = sizeof(struct sockaddr_in);
-        new_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, sin_size);
-        return 0;
+        sin_size = sizeof(struct sockaddr_in);
+        new_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
+        if (new_sockfd == -1)
+            printf("accepting connection");
+        
+        printf("server: got connection from %s port %d\n",
+            inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        send(new_sockfd, "Hello, world!\n", 13, 0);
+        recv_length = recv(new_sockfd, &buffer, 1024, 0);
+        while (recv_length > 0) 
+        {
+            printf("RECV: %d bytes\n", recv_length);
+            dump(buffer, recv_length);
+            recv_length = recv(new_sockfd, &buffer, 1024, 0);
+        }
+        shutdown(new_sockfd, SHUT_RDWR);
     }
 
     // closing the listening socket
